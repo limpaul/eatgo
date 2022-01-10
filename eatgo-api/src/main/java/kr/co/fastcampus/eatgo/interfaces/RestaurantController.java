b72fd0e2eff4df54deb554a5c445c8e823b35f5f
= <=>
2022-01-10 11:57:47 +0900

    what is repository, what is dependency injection

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,16 +1,29 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 @RestController
 public class RestaurantController {
-    @GetMapping("/restaurant")
+
+    @Autowired
+    private RestaurantRepositoryImpl repository;
+
+    @GetMapping("/restaurants")
     public List<Restaurant> list(){
-        List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
-        restaurants.add(restaurant);
+        /*List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L,"Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L,"Cyber Food", "Seoul"));*/
+        List<Restaurant> restaurants = repository.findAll();
         return restaurants;
+    }
+    @GetMapping("/restaurant/{id}")
+    public Restaurant restaurant(@PathVariable("id") Long id){
+        Restaurant restaurant = repository.findById(id);
+        return restaurant;
     }
 }
--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,41 +1,65 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 //@SpringBootTest // 모든 빈을 올리는 통합테스트, 무겁다.
-
 @WebMvcTest(RestaurantController.class) // controller를 테스트
 @AutoConfigureWebMvc //MVC와 관련된 Bean을 올린다
 class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
+
+    // 같은 방식으로 작동하는 객체를 관리할 수 있다.
+    @SpyBean(RestaurantRepositoryImpl.class) // controller에 원하는 bean을 등록해줄 수 있다.
+    private RestaurantRepository repository;
+
     @Test
     public void list() throws Exception{
-        mvc.perform(MockMvcRequestBuilders.get("/restaurant"))
+        mvc.perform(MockMvcRequestBuilders.get("/restaurants"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("Bob zip")));
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("Bob zip")))
+                .andDo(MockMvcResultHandlers.print());
     }
     @Test
     public void list_1() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/restaurant"))
+        mvc.perform(MockMvcRequestBuilders.get("/restaurants"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$..name").value("Bob zip"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$..information").value("Bob zip in Seoul"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$..id").value(1004))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]..name").value("Bob zip"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]..information").value("Bob zip in Seoul"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]..id").value(1004))
                 .andDo(MockMvcResultHandlers.print());
     }
-    
+    @Test
+    public void detail() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/restaurant/1004"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$..id").value(1004))
+                .andExpect(MockMvcResultMatchers.jsonPath("$..name").value("Bob zip"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$..address").value("Seoul"))
+                .andDo(MockMvcResultHandlers.print());
+
+        mvc.perform(MockMvcRequestBuilders.get("/restaurant/2020"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$..id").value(2020))
+                .andExpect(MockMvcResultMatchers.jsonPath("$..name").value("Cyber Food"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$..address").value("Seoul"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
 }
+/*
+* 의존성 주입을 하면 각각의 객체들의 강하게 연결되어있는 관계를 좀 더 유연하게 바꿀 수 있다.
+* 객체를 직접 생성하고 관리해야한는 것을 spring이 해줘서 좀 더 business logic에 신경 쓸 수 있다.
+* */
